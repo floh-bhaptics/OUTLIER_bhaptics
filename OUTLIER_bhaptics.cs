c1631e526e5e90b4db6484f1af9bdf3d785c9c2a
floh-bhaptics <floh@fahrenbergers.de>
2022-03-18 10:15:54 +0100

    Projektdateien hinzufügen.

---
+++ OUTLIER_bhaptics.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MelonLoader;
+using HarmonyLib;
+using MyBhapticsTactsuit;
+
+namespace OUTLIER_bhaptics
+{
+    public class OUTLIER_bhaptics : MelonMod
+    {
+        public static TactsuitVR tactsuitVr;
+
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+            tactsuitVr = new TactsuitVR();
+            tactsuitVr.PlaybackHaptics("HeartBeat");
+        }
+
+        #region Health
+        
+        [HarmonyPatch(typeof(Health), "GetCurrentValue")]
+        public class bhaptics_GetHealth
+        {
+            [HarmonyPostfix]
+            public static void Postfix(Health __instance)
+            {
+                if (__instance.curHealth <= 0.2f * __instance.baseMaxHealth) tactsuitVr.StartHeartBeat();
+                else tactsuitVr.StopHeartBeat();
+            }
+        }
+        
+        [HarmonyPatch(typeof(Player), "Die", new Type[] {  })]
+        public class bhaptics_Death
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                tactsuitVr.StopThreads();
+            }
+        }
+        
+        [HarmonyPatch(typeof(Health), "Heal", new Type[] { typeof(float) })]
+        public class bhaptics_Heal
+        {
+            [HarmonyPostfix]
+            public static void Postfix(Health __instance)
+            {
+                tactsuitVr.PlaybackHaptics("Healing");
+                if (__instance.curHealth >= 0.2f * __instance.baseMaxHealth) tactsuitVr.StopHeartBeat();
+            }
+        }
+        
+        #endregion
+        
+        [HarmonyPatch(typeof(ShootFromInput), "MakeShoot", new Type[] { typeof(bool) })]
+        public class bhaptics_Shoot
+        {
+            [HarmonyPostfix]
+            public static void Postfix(ShootFromInput __instance)
+            {
+                tactsuitVr.Recoil("Gun", (__instance.hand == HandType.Right));
+            }
+        }
+
+        [HarmonyPatch(typeof(DashDetector), "SuccessCast", new Type[] {  })]
+        public class bhaptics_Dash
+        {
+            [HarmonyPostfix]
+            public static void Postfix(DashDetector __instance)
+            {
+                if (!__instance.isDashing) return;
+                tactsuitVr.PlaybackHaptics("SwooshUp");
+            }
+        }
+
+        [HarmonyPatch(typeof(DamageUI), "ShowUI", new Type[] { typeof(DamageEventStatus) })]
+        public class bhaptics_ShowPain
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                tactsuitVr.PlaybackHaptics("Impact");
+            }
+        }
+
+        /*
+        [HarmonyPatch(typeof(EffectManager), "AddInflictionEffect", new Type[] { typeof(TargetHitInfo), typeof(DamageType) })]
+        public class bhaptics_OnHit
+        {
+            [HarmonyPostfix]
+            public static void Postfix(EffectManager __instance, TargetHitInfo _info, DamageType _settingsType)
+            {
+                if (__instance == null) { tactsuitVr.LOG("Instance"); return; }
+                if (_info == null) { tactsuitVr.LOG("THI"); return; }
+                if (_info.hits == null) { tactsuitVr.LOG("THI.Hits"); return; }
+                if (__instance.transform == null) { tactsuitVr.LOG("Instance transform"); return; }
+                if (_settingsType == DamageType.Pure) return;
+                try
+                {
+                    foreach (HitInfo myHit in _info.hits)
+                    {
+                        if (myHit == null) { tactsuitVr.LOG("myHit"); return; }
+                        //tactsuitVr.LOG("Hit: " + __instance.settings.type.ToString());
+                        tactsuitVr.LOG("Hitpoint " + myHit.point.x.ToString() + " " + __instance.transform.position.x.ToString());
+                    }
+
+                }
+                catch (Exception) { }
+                
+                tactsuitVr.PlaybackHaptics("Impact");
+            }
+        }
+        */
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("OUTLIER_bhaptics")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OUTLIER_bhaptics")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+[assembly: MelonInfo(typeof(OUTLIER_bhaptics.OUTLIER_bhaptics), "OUTLIER_bhaptics", "1.0.0", "Florian Fahrenberger")]
+[assembly: MelonGame("JoyWay", "Outlier")]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("6b3c3d7c-674a-4fd0-9f23-f0bebd371ab3")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
